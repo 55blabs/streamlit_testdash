2fb29ae7081ee94b45402926ca6541eeee3da3f8
2the2ndpower <71827677+55blabs@users.noreply.github.com>
2022-03-17 11:28:18 -0700

    Update janDashboard.py

--- janDashboard.py
+++ janDashboard.py
@@ -3,8 +3,10 @@ import pandas as pd
 import numpy as np
 
 
+
 st.header("Michaels Dashboard!")
 st.subheader("Activity for 30 day observation")
+st.write("Demo dashboard that displays daily activity measures in each activity", "Date & Duration")
 
 dataframe = np.random.randn(10, 20)
 st.dataframe(dataframe)
@@ -18,3 +20,6 @@ dataframe = pd.DataFrame(
   np.random.randn(10, 20),
   columns= ('col %d' % i for i in range(20)))
 st.table(dataframe)
+
+chart_data = pd.DataFrame(
+  np.random
