ebccfc83bc6c401bf38eec3d5b0b95baf906db9c
2the2ndpower <71827677+55blabs@users.noreply.github.com>
2022-03-17 10:53:16 -0700

    Update janDashboard.py

--- janDashboard.py
+++ janDashboard.py
@@ -8,3 +8,8 @@ st.subheader("Activity for 30 day observation")
 
 dataframe = np.random.randn(10, 20)
 st.dataframe(dataframe)
+
+dataframe = pd.DataFrame(
+  np.random.randn(10, 20) ,
+  columns =('col %d' % i for i in range(20)))
+st.dataframe(dataframe.style.highlight_max(axis=0))
