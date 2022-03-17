0b7c9ec5dd5c65b60c91eb2f8a2b753acaaaa15f
2the2ndpower <71827677+55blabs@users.noreply.github.com>
2022-03-17 10:50:11 -0700

    Update janDashboard.py

--- janDashboard.py
+++ janDashboard.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
 
 st.header("Michaels Dashboard!")
 st.subheader("Activity for 30 day observation")
+
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
