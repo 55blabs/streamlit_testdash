dba76c9b8d57ee09014747143f305254ccd7910f
2the2ndpower <71827677+55blabs@users.noreply.github.com>
2022-03-17 10:33:43 -0700

    Update janDashboard.py

--- janDashboard.py
+++ janDashboard.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
 
 
-st.write("Hello World")
+st.title("Welcome to Michaels January Activity Dashboard!")
+st.subheader("This application will display data that has been collected on Mr.McNair about his daily lifestyle behaviors over
+             "30 days"
