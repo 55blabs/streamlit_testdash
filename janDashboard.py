53fbc0c7ef3b7fa805f7acadd2360fa02847080c
2the2ndpower <71827677+55blabs@users.noreply.github.com>
2022-03-17 10:34:34 -0700

    Update janDashboard.py

--- janDashboard.py
+++ janDashboard.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 
 st.title("Welcome to Michaels January Activity Dashboard!")
-st.subheader("This application will display data that has been collected on Mr.McNair about his daily lifestyle behaviors over"
-             "30 days"
+st.subheader("This application will display data that has been collected on Mr.McNair about his daily lifestyle behaviors over 30 days"
+            
