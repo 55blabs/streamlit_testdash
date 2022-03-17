5f809d46387fc8a2db22d721f6c3014e2459a90b
2the2ndpower <71827677+55blabs@users.noreply.github.com>
2022-03-17 13:55:57 -0700

    Update janDashboard.py

--- janDashboard.py
+++ janDashboard.py
@@ -14,20 +14,20 @@ print(df)
 
 
 
-dataframe = np.random.randn(10, 20)
-st.dataframe(dataframe)
+#dataframe = np.random.randn(10, 20)
+#st.dataframe(dataframe)
 
-dataframe = pd.DataFrame(
-  np.random.randn(10, 20) ,
-  columns =('col %d' % i for i in range(20)))
-st.dataframe(dataframe.style.highlight_max(axis=0))
+#dataframe = pd.DataFrame(
+  #np.random.randn(10, 20) ,
+  #columns =('col %d' % i for i in range(20)))
+#st.dataframe(dataframe.style.highlight_max(axis=0))
 
-dataframe = pd.DataFrame(
-  np.random.randn(10, 20),
-  columns= ('col %d' % i for i in range(20)))
-st.table(dataframe)
+#dataframe = pd.DataFrame(
+  #np.random.randn(10, 20),
+  #columns= ('col %d' % i for i in range(20)))
+#st.table(dataframe)
 
-chart_data = pd.DataFrame(
-  np.random.randn(20, 3),
-  columns=['a', 'b', 'c'])
-st.line_chart(chart_data)
+#chart_data = pd.DataFrame(
+  #np.random.randn(20, 3),
+  #columns=['a', 'b', 'c'])
+#st.line_chart(chart_data)
