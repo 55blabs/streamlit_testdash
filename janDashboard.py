a8bfe0caa103d5c3e0a2eba975333506a0010b3c
2the2ndpower <71827677+55blabs@users.noreply.github.com>
2022-03-17 14:08:05 -0700

    Update janDashboard.py

--- janDashboard.py
+++ janDashboard.py
@@ -9,24 +9,23 @@ st.subheader("Activity for 30 day observation")
 st.write("How much time are we spending in 10 activities during the time of 30 days")
 st.write(['Duration', 'Date', 'Activity'])
 
-df= pd.read_csv("/Users/michaelmcnair/Desktop/MBD/Januarydata.csv/")
-print(df)
 
 
-#dataframe = np.random.randn(10, 20)
-#st.dataframe(dataframe)
 
-#dataframe = pd.DataFrame(
-  #np.random.randn(10, 20) ,
-  #columns =('col %d' % i for i in range(20)))
-#st.dataframe(dataframe.style.highlight_max(axis=0))
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
 
-#dataframe = pd.DataFrame(
-  #np.random.randn(10, 20),
-  #columns= ('col %d' % i for i in range(20)))
-#st.table(dataframe)
+dataframe = pd.DataFrame(
+  np.random.randn(10, 20) ,
+  columns =('col %d' % i for i in range(20)))
+st.dataframe(dataframe.style.highlight_max(axis=0))
 
-#chart_data = pd.DataFrame(
-  #np.random.randn(20, 3),
-  #columns=['a', 'b', 'c'])
-#st.line_chart(chart_data)
+dataframe = pd.DataFrame(
+  np.random.randn(10, 20),
+  columns= ('col %d' % i for i in range(20)))
+st.table(dataframe)
+
+chart_data = pd.DataFrame(
+  np.random.randn(20, 3),
+  columns=['a', 'b', 'c'])
+st.line_chart(chart_data)
